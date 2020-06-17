f29e76fc7c3c4785910083a99e326006b601134d
Patrick Joel <joekenpat@gmail.com>
2020-06-17 13:59:11 +0100

    ran 1st npm dev & structured user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,37 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+          $table->uuid('id')->primary()->unique();
+          $table->string('first_name');
+          $table->string('last_name');
+          $table->string('username')->unique();
+          $table->string('phone')->unique();
+          $table->string('email')->unique();
+          $table->string('avatar')->nullable()->default(null);
+          $table->enum('gender',['male','female']);
+          $table->string('country');
+          $table->boolean('verification_status')->default(false);
+          $table->string('city')->nullable()->default(null);
+          $table->enum('role',['user','agent','admin','super_admin'])->default('user');
+          $table->string('facebook')->nullable()->unique()->default(null);
+          $table->string('instagram')->nullable()->unique()->default(null);
+          $table->string('google')->nullable()->unique()->default(null);
+          $table->string('twitter')->nullable()->unique()->default(null);
+          $table->string('oauth_provider')->nullable()->unique()->default(null);
+          $table->string('oauth_link')->nullable()->unique()->default(null);
+          $table->string('oauth_id')->nullable()->unique()->default(null);
+          $table->ipAddress('last_ip');
+          $table->string('password');
+          $table->string('address')->nullable()->default(null);
+          $table->text('bio')->nullable()->default(null);
+          $table->rememberToken();
+          $table->timestamp('last_login',6)->nullable()->default(null);
+          $table->timestamp('activated_at',6)->nullable()->default(null);
+          $table->timestamp('blocked_at',6)->nullable()->default(null);
+          $table->timestamp('email_verified_at', 6)->nullable();
+          $table->timestamp('created_at',6)->nullable()->default(null);
+          $table->timestamp('updated_at',6)->nullable()->default(null);
+          $table->timestamp('deleted_at',6)->nullable()->default(null);
         });
     }
 
