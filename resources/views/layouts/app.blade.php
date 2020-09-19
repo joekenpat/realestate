798c398c618c9300ceda693995598636a29c8971
Patrick Joel <joekenpat@gmail.com>
2020-09-19 18:31:30 +0100

    android status bar and microsoft tile color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <meta name="theme-color" content="#000000">
+  <meta name="msapplication-TileColor" content="#000000">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="description" content="{{ config('app.name', 'MyNextLand') }} - Buy and Sell Lands, Homes, and Apartments">
