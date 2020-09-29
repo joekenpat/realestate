ac353b502120f9ce235c5047d8a05a3c053dd77a
Patrick Joel <joekenpat@gmail.com>
2020-09-29 11:36:48 +0100

    server quil snow theme css locally

--- resources/views/property/edit.blade.php
+++ resources/views/property/edit.blade.php
@@ -206,7 +206,7 @@
     object-fit: cover;
   }
 </style>
-<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+<link href="{{asset('css/quill.snow.css')}}" rel="stylesheet">
 @endpush
 <div class="uk-grid-collapse" uk-grid>
 
