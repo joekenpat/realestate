895860bf036ff2a0abe0350142bb795ab94cda04
Patrick Joel <joekenpat@gmail.com>
2020-10-05 18:00:22 +0100

    fixed classname confliction

--- database/migrations/2020_9_23_102142_add_referer_column_to_table.php
+++ database/migrations/2020_9_23_102142_add_referer_column_to_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRefererColumnToUsers extends Migration
+class AddRefererColumnToTable extends Migration
 {
   /**
    * Run the migrations.
