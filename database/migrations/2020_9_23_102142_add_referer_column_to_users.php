30f8211b59afe1422557435241a0cfef5fc86d03
Patrick Joel <joekenpat@gmail.com>
2020-10-05 17:46:56 +0100

    customized verification page

--- database/migrations/2020_9_23_102142_add_referer_column_to_users.php
+++ database/migrations/2020_9_23_102142_add_referer_column_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSlugColumnToUsers extends Migration
+class AddReferColumnToUsers extends Migration
 {
   /**
    * Run the migrations.
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.app')
-
+@section('title', "Verify Your Email")
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                    <div class="card-body">
-                        @if (session('resent'))
-                            <div class="alert alert-success" role="alert">
-                                {{ __('A fresh verification link has been sent to your email address.') }}
-                            </div>
-                        @endif
-
-                        {{ __('Before proceeding, please check your email for a verification link.') }}
-                        {{ __('If you did not receive the email') }},
-                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                            @csrf
-                            <button type="submit"
-                                    class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
-                            .
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="uk-section uk-section-small uk-section-muted uk-flex uk-flex-center">
+  <div class="uk-card uk-card-default my-card uk-card-body uk-width-large">
+    <h2 class="uk-card-title" style="color:#3D9FB9;">{{ __('Verify Your Email Address') }}</h2>
+    @if (session('resent'))
+    <div class="uk-alert-success" uk-alert>
+      <a class="uk-alert-close" uk-close></a>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
     </div>
+    @endif
+    <p class=>
+      {{ __('Before proceeding, please check your email for a verification link.') }}
+      {{ __('If you did not receive the email') }},
+      <form class="d-uk-form-stacked" method="POST" action="{{ route('verification.resend') }}">
+        @csrf
+        <div class="uk-margin">
+          <div class="uk-form-control">
+            <button type="submit" class="uk-button uk-width-1-1 white-text" style="background-color:#3D9FB9;">
+              Resend Another Now
+            </button>
+          </div>
+        </div>
+      </form>
+    </p>
+  </div>
+</div>
+</div>
+</div>
+</div>
 @endsection
